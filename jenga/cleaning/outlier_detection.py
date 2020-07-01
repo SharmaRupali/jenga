53dda4a7c27aaa167f9e007e4fba4d465cfe7f88
SharmaRupali <rups1501@gmail.com>
2020-07-01 23:56:25 +0200

    updated pyod Isolation Forest to use the same as knn + removed the code comments

--- jenga/cleaning/outlier_detection.py
+++ jenga/cleaning/outlier_detection.py
@@ -119,20 +119,22 @@ class PyODKNN(PyodGeneralOutlierDetection):
 
     
     
-class PyODIsolationForest(OutlierDetection):
+class PyODIsolationForest(PyodGeneralOutlierDetection):
     
     def fit_transform(self, df_train, df_corrupted):
-        df_outliers = df_corrupted.copy()
-        
-        feature_transformation = self.feature_transform.fit(df_train)
-        x = feature_transformation.transform(df_train).toarray()
-        
-        model = IForest(contamination=0.25)
-        model.fit(x)
-        
-        xx = feature_transformation.transform(df_outliers).toarray()
+        pyod_model = IForest(contamination=0.25)
 
-        df_outliers["outlier"] = model.predict(xx) ## 0: inlier, 1: outlier
+        df_outliers_num = self.num_out_detect(df_train, df_corrupted, pyod_model)
+        df_outliers_cat = self.cat_out_detect(df_train, df_corrupted)
+
+        df_outliers = df_outliers_num.join(df_outliers_cat, how='inner')
+
+        for col in df_corrupted.columns:
+            for i in df_outliers.index:
+                if df_outliers[col + "_outlier"].loc[i] == 1:
+                    df_outliers[col].loc[i] = np.nan
+
+        df_outliers = df_outliers[df_corrupted.columns]
         
         return df_outliers
     
@@ -140,35 +142,3 @@ class PyODIsolationForest(OutlierDetection):
     def __call__(self, df_train, df_corrupted):
         return self.fit_transform(df_train, df_corrupted)
 
-
-
-# def pyod_num_out_detect(self, df_train, df_corrupted, pyod_model):
-#     df_outliers = df_corrupted[self.numerical_columns].copy()
-    
-#     for col in df_train.columns:
-#         if col in self.numerical_columns:
-#             ## find indices of records with NaNs in col in df_corrupted
-#             nan_idx = df_corrupted[df_corrupted[col].isnull()].index
-#             non_nan_idx = df_corrupted.loc[set(df_corrupted.index) - set(nan_idx)].index
-            
-#             ## pd series -> np column, needs to be 2D array
-#             ## taking only the non-NaN records in the corrupted data
-#             col_tr_arr = np.array(df_train[col]).reshape(-1,1)
-#             col_corr_arr = np.array(df_corrupted.loc[non_nan_idx][col]).reshape(-1,1)
-
-#             ## fit the dataset to the model
-#             model = pyod_model
-#             model.fit(col_tr_arr)
-
-#             ## predict raw anomaly score
-#             scores_pred = model.decision_function(col_corr_arr) * -1
-
-#             ## prediction of a datapoint category outlier or inlier
-#             y_pred = model.predict(col_corr_arr)
-
-#             ## add a respective outlier col for each col
-#             df_outliers[col + "_outlier"] = ''
-#             df_outliers[col + "_outlier"].loc[non_nan_idx] = y_pred ## 0: inlier, 1: outlier
-#             df_outliers[col + "_outlier"].loc[nan_idx] = 0
-            
-#     return df_outliers 
