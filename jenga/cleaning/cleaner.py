47794d0faa149efe46c0b27f917ae11f80c1f364
SharmaRupali <rups1501@gmail.com>
2020-09-08 15:46:42 +0200

    added methods for calculating outlier detection scores and imputation scores respectively

--- jenga/cleaning/cleaner.py
+++ jenga/cleaning/cleaner.py
@@ -1,5 +1,9 @@
+import numpy as np
+
 from jenga.cleaning.outlier_detection import NoOutlierDetection
 from jenga.cleaning.imputation import NoImputation
+
+from sklearn.metrics import classification_report, accuracy_score, mean_squared_error
 
 
 class Cleaner:
@@ -8,9 +12,12 @@ class Cleaner:
         self.outlier_detection = outlier_detection
         self.imputation = imputation
 
+        self.categorical_columns = categorical_columns
+        self.numerical_columns = numerical_columns
+
 
     def __repr__(self):
-        return f"{self.__class__.__name__}: {self.__dict__}"
+        return f"{self.__class__.__name__}: {self.outlier_detection, self.imputation}"
         
     
     def apply_cleaner(self, df_train, df_corrupted):
@@ -18,6 +25,106 @@ class Cleaner:
         df_outliers, predictors, predictable_cols = self.outlier_detection(df_train, df_corrupted)
 
         # impute
-        df_imputed = self.imputation(df_train, df_outliers, predictors)
+        df_imputed = self.imputation(df_train, df_outliers[df_corrupted.columns], predictors)
         
-        return df_imputed
+        return df_outliers, df_imputed
+
+
+    def cleaner_scores(self, df_test, df_corrupted, df_outliers, df_cleaned, cols_perturbed):
+        ## y_true is the column value from the original test data
+        
+        outlier_detection_score = self.outlier_detection_scores(df_test, df_corrupted, df_outliers, cols_perturbed)
+        imputation_score = self.imputation_scores(df_test, df_cleaned, cols_perturbed)
+
+        return outlier_detection_score, imputation_score
+
+
+    def imputation_scores(self, df_test, df_cleaned, cols_perturbed):
+        classif_reports = {}
+
+        acc_scores = []
+        f1socres = []
+        recallscores = []
+        precisionscores = []
+
+        mse = []
+
+        for col in cols_perturbed:
+            if col in self.categorical_columns:
+                classif_reports[col] = classification_report(df_test[col], df_cleaned[col], output_dict=True)
+
+                labels = [k for k in classif_reports[col] if k not in ['accuracy', 'macro avg', 'weighted avg']]
+
+                f1s = []
+                res = []
+                pres = []
+
+                for label in labels:
+                    f1s.append(classif_reports[col][label]['f1-score'])
+                    res.append(classif_reports[col][label]['recall'])
+                    pres.append(classif_reports[col][label]['precision'])
+
+                f1socres.append(np.mean(f1s))
+                recallscores.append(np.mean(res))
+                precisionscores.append(np.mean(pres))
+
+                acc_scores.append(accuracy_score(df_test[col], df_cleaned[col]))
+            else:
+                mse.append(mean_squared_error(df_test[col], df_cleaned[col]))
+
+        imputation_scores_summ = {
+            "Precision": np.mean(precisionscores),
+            "Recall": np.mean(recallscores),
+            "F1-score": np.mean(f1socres),
+            "Accuracy": np.mean(acc_scores),
+            "Mean Squared Error": np.mean(mse)
+        }
+
+        return imputation_scores_summ
+
+
+    def outlier_detection_scores(self, df_test, df_corrupted, df_outliers, cols_perturbed):
+        df_test_out = df_test.copy()
+
+        classif_reports = {}
+        acc_scores = []
+        f1socres = []
+        recallscores = []
+        precisionscores = []
+
+        for col in cols_perturbed:
+            outiers_man = np.equal(df_test[col], df_corrupted[col])
+            outiers_man_ind = outiers_man.index[outiers_man == False]
+
+            non_outliers_man_ind = df_test_out.loc[set(df_test_out.index) - set(outiers_man_ind)].index
+
+            df_test_out.loc[outiers_man_ind, col + "_outlier"] = 1 ## outliers
+            df_test_out.loc[non_outliers_man_ind, col + "_outlier"] = 0 ## not outliers
+
+            classif_reports[col] = classification_report(df_test_out[col + "_outlier"], df_outliers[col + "_outlier"], output_dict=True)
+
+            labels = [k for k in classif_reports[col] if k not in ['accuracy', 'macro avg', 'weighted avg']]
+
+            f1s = []
+            res = []
+            pres = []
+
+            for label in labels:
+                f1s.append(classif_reports[col][label]['f1-score'])
+                res.append(classif_reports[col][label]['recall'])
+                pres.append(classif_reports[col][label]['precision'])
+
+            f1socres.append(np.mean(f1s))
+            recallscores.append(np.mean(res))
+            precisionscores.append(np.mean(pres))
+
+            acc_scores.append(accuracy_score(df_test_out[col + "_outlier"], df_outliers[col + "_outlier"]))
+
+        od_scores_summ = {
+            "Precision": np.mean(precisionscores),
+            "Recall": np.mean(recallscores),
+            "F1-score": np.mean(f1socres),
+            "Accuracy": np.mean(acc_scores)
+        }
+
+        return od_scores_summ
